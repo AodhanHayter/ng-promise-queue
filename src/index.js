6b18e9e58532e3bca800d466ed2d89022891d6f7
Aodhan Hayter <aodhan.hayter@gmail.com>
2017-09-17 13:57:10 -0600

    Remove all non gh-pages related files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular'
-import promiseQueue from './factories/promiseQueue.js'
-
-export default angular
-  .module('ngPromiseQueue', [])
-  .factory('promiseQueue', promiseQueue).name
